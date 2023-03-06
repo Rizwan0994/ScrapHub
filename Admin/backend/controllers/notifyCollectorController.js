98da55f5dfeaaa8f812380597c887b982b61aa30
UmairAkram948 <f190948@nu.edu.pk>
2023-03-06 19:39:46 +0500

    notifycollector by Admin

--- Admin/backend/controllers/notifyCollectorController.js
+++ Admin/backend/controllers/notifyCollectorController.js
@@ -1,37 +1,39 @@
-const Pickup = require('..//models/notifycollector');
+const Pickup = require('../models/notifycollector');
 
 class PickupController {
-  static async create(req, res) {
-    try {
-      const {
-        contact,
-        Address,
-        nearestYard,
-        sDate,
-        sTime,
-        itemDetails,
-        collectorYard,
-        collectorId,
-      } = req.body;
+  constructor() {
+    this.create = async (req, res) => {
+      try {
+        const {
+          contact,
+          Address,
+          nearestYard,
+          sDate,
+          sTime,
+          itemDetails,
+          collectorYard,
+          collectorId,
+        } = req.body;
 
-      const pickup = new Pickup({
-        contact,
-        Address,
-        nearestYard,
-        sDate,
-        sTime,
-        itemDetails,
-        collectorYard,
-        collectorId,
-      });
+        const pickup = new Pickup({
+          contact,
+          Address,
+          nearestYard,
+          sDate,
+          sTime,
+          itemDetails,
+          collectorYard,
+          collectorId,
+        });
 
-      await pickup.save();
+        await pickup.save();
 
-      res.status(201).json({ message: 'Pickup created successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
+        res.status(201).json({ message: 'Pickup created successfully' });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    };
   }
 }
 
--- Admin/backend/routes/notifyCollectorRoute.js
+++ Admin/backend/routes/notifyCollectorRoute.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const protect = require("../middleWare/authMiddleware");
 const addNotifyController = require("../controllers/notifyCollectorController");
 
-router.post("/", addNotifyController);
+const pickupController = new addNotifyController();
+// Route to handle creating a new pickup
+router.post('/', async (req, res) => {
+    await pickupController.create(req, res);
+  });
+
 
 module.exports = router;
