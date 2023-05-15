bbb6b87183cd19b301db412c517d50ee345eb982
Rizwan0994 <f190994@nu.edu.pk>
2023-05-16 00:40:55 +0500

    set routes

--- Buyer/app-frontend/src/App.js
+++ Buyer/app-frontend/src/App.js
@@ -4,6 +4,9 @@ import {Routes, Route} from "react-router-dom";
 import  PaymentPage from "./components/PaymentPage"
 import ProductPage from "./components/ProductPage"
 import MainPage from "./components/MainPage"
+import FeedbackForm from './components/FeedbackForm';
+import SuccessPage from "./components/SuccessPage";
+import FailurePage from "./components/FailurePage";
 function App() {
   return(
     <div className="App">
@@ -11,6 +14,9 @@ function App() {
       <Route path="/payment"  element={<PaymentPage/>} />
       <Route path="/"  element={<ProductPage/>} />
       <Route path="/main"  element={<MainPage/>} />
+      <Route path="/feedback"  element={<FeedbackForm/>} />
+      <Route path="/success"  element={<SuccessPage/>} />
+      <Route path="/failure"  element={<FailurePage/>} />
     </Routes>
     </div>
   )
